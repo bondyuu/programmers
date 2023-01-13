6edf74a79bcbb0775f6608f39a9e14f56d903e8f
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-01-13 11:34:23 +0900

    Add files via upload

---
+++ 124나라의 숫자/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int n) {
+        String[] numbers = {"4", "1", "2"};
+        String answer = "";
+
+        int num = n;
+
+        while(num > 0){
+            int remainder = num % 3;
+            num /= 3;
+
+            if(remainder == 0) num--;
+
+            answer = numbers[remainder] + answer;
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(10));
+    }
+}
