3e5203185dccdc15af4c2e456ebb8e479d5d8948
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-01-13 12:22:28 +0900

    Add files via upload

---
+++ 타겟 넘버/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int idx, int target, int sum) {
+        if(idx == numbers.length) {
+            if (target == sum) {
+                answer++;
+                }
+        } else {
+            dfs(numbers, idx + 1, target, sum + numbers[idx]);
+            dfs(numbers, idx +1, target, sum - numbers[idx]);
+        }
+    }
+}
