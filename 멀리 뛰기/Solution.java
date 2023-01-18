f4e3d8833085ca091c92dc1c76e6f18493ff42b0
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-01-18 11:54:14 +0900

    Add files via upload

---
+++ 멀리 뛰기/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int n) {
+        int[] dp = new int[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<2001; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(2000));
+    }
+}
