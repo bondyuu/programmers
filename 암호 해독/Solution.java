30686a4e7b5a412951d2c30c60af13ba5363dacf
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-01-26 14:44:30 +0900

    Add files via upload

---
+++ 암호 해독/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for (int i = code; i <= cipher.length(); i += code) {
+            answer += String.valueOf(cipher.charAt(i-1));
+        }
+
+        return answer;
+    }
+}
