3cc0e309221ad252be64d697f388f19728be0e0c
bondyuu <87316155+bondyuu@users.noreply.github.com>
2023-02-08 22:01:34 +0900

    Add files via upload

---
+++ OX퀴즈/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for (int i = 0; i < quiz.length; i++) {
+            String[] arr = quiz[i].split(" ");
+            if (arr[1].equals("+")) {
+                if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            } else if (arr[1].equals("-")) {
+                if (Integer.parseInt(arr[0]) - Integer.parseInt(arr[2]) == Integer.parseInt(arr[4])) {
+                    answer[i] = "O";
+                } else {
+                    answer[i] = "X";
+                }
+            }
+        }
+        return answer;
+    }
+}
